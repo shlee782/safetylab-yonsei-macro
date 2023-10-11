3de9dd4cfe3e32743bcef99b914e9481c6111ea6
Sunghwa Lee <43434107+shlee782@users.noreply.github.com>
2023-10-11 13:11:25 +0900

    Update (ChromeDriver is not required.)

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import pandas as pd
 from math import ceil
 
 import selenium.webdriver as webdriver
-# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 from datetime import datetime
